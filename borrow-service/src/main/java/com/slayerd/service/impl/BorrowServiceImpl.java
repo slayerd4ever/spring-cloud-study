060a9a7dafe176b3a33f269c034dffb087155232
slayerd <syd19970616@gmail.com>
2022-06-23 16:42:41 +0800

    完成 eureka集群

--- borrow-service/src/main/java/com/slayerd/service/impl/BorrowServiceImpl.java
+++ borrow-service/src/main/java/com/slayerd/service/impl/BorrowServiceImpl.java
@@ -6,6 +6,7 @@ import com.slayerd.entity.User;
 import com.slayerd.entity.UserBorrowDetail;
 import com.slayerd.mapper.BorrowMapper;
 import com.slayerd.service.BorrowService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -19,18 +20,18 @@ public class BorrowServiceImpl implements BorrowService {
     @Resource
     BorrowMapper mapper;
 
+    @Autowired
+    RestTemplate restTemplate;
     @Override
     public UserBorrowDetail getUserBorrowDetailByUid(int uid) {
         List<Borrow> borrow = mapper.getBorrowsByUid(uid);
-        //RestTemplate支持多种方式的远程调用
-        RestTemplate template = new RestTemplate();
         //这里通过调用getForObject来请求其他服务，并将结果自动进行封装
         //获取User信息
-        User user = template.getForObject("http://localhost:8101/user/"+uid, User.class);
+        User user = restTemplate.getForObject("http://USERSERVICE/user/"+uid, User.class);
         //获取每一本书的详细信息
         List<Book> bookList = borrow
                 .stream()
-                .map(b -> template.getForObject("http://localhost:8201/book/"+b.getBid(), Book.class))
+                .map(b -> restTemplate.getForObject("http://BOOKSERVICE/book/"+b.getBid(), Book.class))
                 .collect(Collectors.toList());
         return new UserBorrowDetail(user, bookList);
     }
--- user-service/src/main/java/com/slayerd/controller/UserController.java
+++ user-service/src/main/java/com/slayerd/controller/UserController.java
@@ -15,7 +15,8 @@ public class UserController {
 
     //这里以RESTFul风格为例
     @RequestMapping("/user/{uid}")
-    public User findUserById(@PathVariable("uid") int uid){
+    public User findUserById(@PathVariable("uid") int uid) {
+        System.out.println("我被调用了");
         return userService.getUserById(uid);
     }
 }
