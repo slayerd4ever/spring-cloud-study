3328f6c619d45f713a999c21a9530494e0577c54
slayerd <syd19970616@gmail.com>
2022-08-09 11:10:47 +0800

    负载均衡的自定义

--- common/src/main/java/com/slayerd/config/BeanConfiguration.java
+++ common/src/main/java/com/slayerd/config/BeanConfiguration.java
@@ -1,11 +1,13 @@
 package com.slayerd.config;
 
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@LoadBalancerClient(value = "USERSERVICE",configuration = LoadBalancerConfig.class)
 public class BeanConfiguration {
     @Bean
     @LoadBalanced
